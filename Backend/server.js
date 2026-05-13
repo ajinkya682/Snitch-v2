ae21d308509756af9a90f7a2acd4ea887853fae7
Ajinkya Saivar <172878214+ajinkya682@users.noreply.github.com>
2026-05-13 16:11:50 +0530

    refactor: remove unused dependencies and clean up code
    - Removed various dependencies from package.json that are no longer needed.
    - Eliminated database connection logic and related configuration files.
    - Deleted user model, authentication controller, and routes as they are not in use.
    - Cleaned up app.js to remove middleware and routes related to authentication.

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,5 @@
 import app from "./src/app.js";
-import connectToDB from "./src/config/database.js";
 
-connectToDB();
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,5 @@
 import express from "express";
-import cokieParser from "cookie-parser";
-
-import authRoute from "./routes/auth.route.js";
 
 const app = express();
-
-app.use(express.json());
-app.use(cokieParser());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/auth", authRoute);
 
 export default app;
